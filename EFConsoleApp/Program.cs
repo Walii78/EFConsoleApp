b8716e6dcb76365f402ca18ec4e90c2753d7bc80
Walii78 <129457570+Walii78@users.noreply.github.com>
2023-08-06 00:21:32 -0700

    Add project files.

---
+++ EFConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EFConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    BirthDate = new DateTime(2000, 1, 1)
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added to the database.");
+            }
+        }
+    }
+}
---
+++ EFConsoleApp/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsoleApp
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-9OK3L209\\SQLEXPRESS");
+        }
+    }
+}
